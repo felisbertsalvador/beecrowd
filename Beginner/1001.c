6eb893b919af2eadd573e4e82cbb56eba9813fbf
Felisberto Salvador <felisbertsalvador@gmail.com>
2022-02-02 18:48:53 +0100

    solution to the problem 1001

---
+++ Beginner/1001.c
@@ -0,0 +1,15 @@
+// Extremely Basic (1001)
+
+#include <stdio.h>
+
+int main(){
+    
+    int A, B;
+    int sum;
+
+    scanf("%d%d", &A, &B);
+    sum = A + B;
+    printf("X = %d\n", sum);
+
+    return 0;
+}
